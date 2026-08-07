2491acd3ed9687e3fc907ca1371d494338d32777
RAYMOND PETERS <raymondpeters117@gmail.com>
2026-08-07 13:44:33 +0300

    Update teacher.js

--- teacher.js
+++ teacher.js
@@ -1,3 +1,14 @@
+const teacher =
+JSON.parse(localStorage.getItem("loggedTeacher"));
+
+
+if(!teacher){
+
+    alert("Please login first.");
+
+    window.location.href="portal.html";
+
+}
 /* =====================================================
    KID'S LOVE NURSERY
    TEACHER DASHBOARD JAVASCRIPT
